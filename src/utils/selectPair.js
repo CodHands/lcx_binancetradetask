5e17d512fad7c6f5269dd56407639d519bed94c5
vinaysingh <vinaysinghbb@gmail.com>
2019-06-03 10:53:51 +0530

    indent changes

--- src/utils/selectPair.js
+++ src/utils/selectPair.js
@@ -3,23 +3,23 @@ import React from 'react';
 const MarketPairs = (props) => {
 
   const handleChange = (symbol) => {
-    props.props.history.push(`/trades/${symbol}`);    
+    props.props.history.push(`/trades/${symbol}`);
   }
 
-  const pairList = () => {    
-    if(props.pairs.length){
-        return props.pairs.slice(0,60).map((pair,index) => {
-          return <div className="col-sm-2" onClick={() => handleChange(pair.symbol)} key={index}>
-                    <div className="card mb-2">
-                        <h6><img src="/images/blockchain.png" width="16" alt="crypto-icon"/>&nbsp;&nbsp;{pair.baseAsset}/{pair.quoteAsset}</h6>
-                    </div>
-                  </div>
-        })
+  const pairList = () => {
+    if (props.pairs.length) {
+      return props.pairs.slice(0, 60).map((pair, index) => {
+        return <div className="col-sm-2" onClick={() => handleChange(pair.symbol)} key={index}>
+          <div className="card mb-2">
+            <h6><img src="/images/blockchain.png" width="16" alt="crypto-icon" />&nbsp;&nbsp;{pair.baseAsset}/{pair.quoteAsset}</h6>
+          </div>
+        </div>
+      })
     } else {
       return <div className="spinner-loader">
-                <img src="/images/loading.gif" alt="loading"/>
-            </div>
-    }  
+        <img src="/images/loading.gif" alt="loading" />
+      </div>
+    }
   }
 
   return (
